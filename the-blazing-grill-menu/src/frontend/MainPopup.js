992fddfef47bb70a0e41f70c2d34e87bdb092d80
kaleem <kaleem1999@outlook.com>
2024-02-06 13:39:30 +0200

    new changes

--- the-blazing-grill-menu/src/frontend/MainPopup.js
+++ the-blazing-grill-menu/src/frontend/MainPopup.js
@@ -4,7 +4,20 @@ import databaseNames from "../database/databaseNames";
 import { useState, useEffect } from "react";
 import MenuItemsSection from "./menuSections";
 const MainPopup = ({ setStoreState, storeState, setId, id }) => {
+  const menuOptions = [
+    { name: "Menu1", selected: false },
+    { name: "Menu2", selected: false },
+    { name: "Menu3", selected: false },
+  ];
+  const otherOptions = [
+    { name: "Edit", selected: false },
+    { name: "View", selected: false },
+  ];
   const [data, setData] = useState([]); // Initialize state for storing data
+  const [store, setStore] = useState(null);
+  const [menu, setMenu] = useState(null);
+  const [mode, setMode] = useState(null);
+
   useEffect(() => {
     const unsubscribe = onSnapshot(
       collection(db, databaseNames[0]),
@@ -30,13 +43,18 @@ const MainPopup = ({ setStoreState, storeState, setId, id }) => {
     };
   }, []);
 
-  const setDataFunct = (store) => {
+  const setDataFunct = () => {
+    if (store === null || menu === null || mode === null) {
+      return alert("Please select ensure to select all options.");
+    }
+    let newMode = `?${mode.name.toLowerCase()}`;
+    if (mode.name.toLowerCase() === "view") {
+      newMode = "";
+    }
     let keyName = Object.keys(store)[0];
     let idName = Object.keys(store)[1];
     setId(store[idName]);
     setStoreState(store[keyName]);
-    console.log(store[idName], "storeState");
-    console.log(store, "storeState");
     localStorage.setItem("storeData", JSON.stringify(store[keyName]));
     localStorage.setItem("storeId", store[idName]);
     let result = [];
@@ -60,15 +78,17 @@ const MainPopup = ({ setStoreState, storeState, setId, id }) => {
 
     setTimeout(() => {
       localStorage.setItem("ITEMS", JSON.stringify(result));
-      window.location.href += "?menu1?edit";
+      window.location.href += "?" + menu.name.toLowerCase() + newMode;
+      //   "?menu1?edit";
     }, 1000);
   };
   return (
     <div className="MainPopup">
-      <h2>Please select a store</h2>
+      <h1>Please select a store</h1>
       <select
+        className="selectMenu"
         onChange={(event) =>
-          setDataFunct(
+          setStore(
             data.find((store) => Object.keys(store)[0] === event.target.value)
           )
         }
@@ -84,6 +104,42 @@ const MainPopup = ({ setStoreState, storeState, setId, id }) => {
           );
         })}
       </select>
+      <h2>Select a menu</h2>
+      <div className="menuAndEditViewOptions">
+        {menuOptions.map((opt) => (
+          <div
+            style={
+              menu !== null && menu.name === opt.name
+                ? { color: "#f7941d" }
+                : {}
+            }
+            onClick={(e) => setMenu({ ...opt, selected: true })}
+            className="menuAndEditViewOptionsItem"
+          >
+            {opt.name}
+          </div>
+        ))}
+      </div>
+      <h2>Select a mode</h2>
+
+      <div style={{ width: "50%" }} className="menuAndEditViewOptions">
+        {otherOptions.map((opt) => (
+          <div
+            style={
+              mode !== null && mode.name === opt.name
+                ? { color: "#f7941d" }
+                : {}
+            }
+            className="menuAndEditViewOptionsItem"
+            onClick={(e) => setMode({ ...opt, selected: true })}
+          >
+            {opt.name}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setDataFunct()} className="EnterButton">
+        Enter
+      </button>
     </div>
   );
 };
